cc8fb013a20d1641a10ac1be97950f6f2c9d9aba
Igor Plastov <chetverovod@gmail.com>
2024-07-29 09:55:04 +0300

    add demo file.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # cbrfparser
-Parser for CBRF  web-page
+Модуль для получения данных с WEB-страницы Центраольного Банка Российской Федерации
+
+Пример использования:
+```python
+import cbrf
+
+data = cbrf.currencies()
+
+for r in data:
+   print('Currency:', r[0], " Yesterday:", r[1], " Today:", r[2])
+  
+```
+ответ:
+```
+Currency: CNY, 1¥  Yesterday: 11,7408 ₽  Today: 11,7296 ₽
+Currency: USD, 1$  Yesterday: 85,4100 ₽  Today: 85,5650 ₽
+Currency: EUR, 1€  Yesterday: 93,1711 ₽  Today: 93,2641 ₽
+
+```
--- cbrf.py
+++ cbrf.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-# In[2]:
-
-
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -13,14 +9,13 @@ from collections import OrderedDict
 import sys
 import html5lib
 
-
-# In[3]:
-
+# CBRF site
+cbrf_url='https://www.cbr.ru/'
 
 # Define a function that retrieves information about currency.
 # a dictionary form.
-def extract_data_quote(quote_html):
-    divs = quote_html.select('div', class_='col-md-2.col-xs-9')
+def extract_data_currency(currency_html):
+    divs = currency_html.select('div', class_='col-md-2.col-xs-9')
     rate=[]
     for div in divs:
         content = div.text.strip()
@@ -29,8 +24,8 @@ def extract_data_quote(quote_html):
     return rate[:-2]
 
 
-# Define a function that retrieves all the quotes on a single page.
-def get_quotes_data(page_url):
+# Define a function that retrieves all the currencies.
+def get_currencies_data(page_url):
     try:
         page = requests.get(page_url)
     except requests.exceptions.Timeout:
@@ -38,20 +33,17 @@ def get_quotes_data(page_url):
         sys.exit(1)
     else:    
         if page.status_code == 200:
-            print(f"Page {page_url} parsing.")
             pageParsed = BeautifulSoup(page.content, 'html5lib')
-            quotes_html_page = pageParsed.find_all('div', {'class': "main-indicator_rate"})
-            res = [extract_data_quote(quote_html) for quote_html in quotes_html_page]
+            currencys_html_page = pageParsed.find_all('div', {'class': "main-indicator_rate"})
+            res = [extract_data_currency(currency_html) for currency_html in currencys_html_page]
             return res 
         else:
             print("Page was not parsed.")
             sys.exit(1)
 
-# CBRF site
-cbrf_url='https://www.cbr.ru/'
 
 def currencies():
     # Retrieve data from the first page.
-    data = get_quotes_data(cbrf_url)
+    data = get_currencies_data(cbrf_url)
     return data
 
