6a9e2f15cddf36880efc7f128aa2c3728a087d6a
Igor Plastov <chetverovod@gmail.com>
2024-07-31 11:51:24 +0300

    Make refactoring.

--- cbrf.py
+++ cbrf.py
@@ -16,44 +16,9 @@ cbrf_indicators_url = 'https://www.cbr.ru/key-indicators/'
 
 cn = ['ru_name', 'name', 'yesterday_rate', 'today_rate']
 
-# Define a function that retrieves information about currency.
-# a dictionary form.
-def extract_data_currency(html):
-    divs = html.select('div', class_='col-md-2.col-xs-9')
-    rate=[]
-    for div in divs:
-        content = div.text.strip()
-        css_class = div.get('class')
-        rate.append(content)
-    return rate[:-2]
-
 
 # Define a function that retrieves all the currencies.
 def get_currencies_data(page_url):
-    try:
-        page = requests.get(page_url)
-    except requests.exceptions.Timeout:
-        print("The request timed out!")
-        sys.exit(1)
-    else:    
-        if page.status_code == 200:
-            pageParsed = BeautifulSoup(page.content, 'html5lib')
-            currencys_html_page = pageParsed.find_all('div', {'class': "main-indicator_rate"})
-            res = [extract_data_currency(currency_html) for currency_html in currencys_html_page]
-            return res 
-        else:
-            print("Page was not parsed.")
-            sys.exit(1)
-
-
-def currencies():
-    # Retrieve data from the first page.
-    data = get_currencies_data2(cbrf_indicators_url)
-    return data
-
-
-# Define a function that retrieves all the metals.
-def get_currencies_data2(page_url):
     try:
         page = requests.get(page_url)
     except requests.exceptions.Timeout:
@@ -101,6 +66,12 @@ def get_currencies_data2(page_url):
             sys.exit(1)
 
 
+# Define a function that retrieves information about currency.
+# a dictionary form.
+def currencies():
+    # Retrieve data from the first page.
+    data = get_currencies_data(cbrf_indicators_url)
+    return data
 
 
 # Define a function that retrieves all the metals.
@@ -162,6 +133,8 @@ def get_metals_data(page_url):
             sys.exit(1)
 
 
+# Define a function that retrieves information about metals.
+# a dictionary form.
 def metals():
     # Retrieve data from the first page.
     data = get_metals_data(cbrf_indicators_url)
