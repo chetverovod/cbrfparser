00d3681439d2bc53e2d4092995cc2c4edee38f5a
Igor Plastov <chetverovod@gmail.com>
2024-08-01 11:47:08 +0300

    Update  tests.

--- cbrf.py
+++ cbrf.py
@@ -3,10 +3,8 @@
 
 import requests
 from bs4 import BeautifulSoup
-import json
 import sys
-import html5lib
-from lxml import etree 
+from lxml import etree
 
 # CBRF site pages.
 cbrf_url = 'https://www.cbr.ru/'
@@ -22,43 +20,55 @@ def get_currencies_data(page_url):
     except requests.exceptions.Timeout:
         print("The request timed out!")
         sys.exit(1)
-    else:    
+    else:
         if page.status_code == 200:
             pageParsed = BeautifulSoup(page.content, 'html5lib')
-            dom = etree.HTML(str(pageParsed)) 
+            dom = etree.HTML(str(pageParsed))
 
             curr = {
-            cn[0]:'//*[@id="content"]/div/div/div/div[2]/div[2]/div[4]/div/div/table/tbody/tr[2]/td[1]/div/div[1]',
-            cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4]/div/div/table/tbody/tr[2]/td[1]/div/div[2]',
-            cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4]/div/div/table/tbody/tr[2]/td[2]',
-            cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4]/div/div/table/tbody/tr[2]/td[3]'
-            }  
+                cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+                    /div/div/table/tbody/tr[2]/td[1]/div/div[1]',
+                cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+                    /div/div/table/tbody/tr[2]/td[1]/div/div[2]',
+                cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+                    /div/div/table/tbody/tr[2]/td[2]',
+                cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+                    /div/div/table/tbody/tr[2]/td[3]'
+            }
             cny = {}
             for k in curr.keys():
                 cny[k] = (dom.xpath(curr[k])[0].text).strip()
 
             curr = {
-            cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4]/div/div/table/tbody/tr[3]/td[1]/div/div[1]', 
-            cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4]/div/div/table/tbody/tr[3]/td[1]/div/div[2]',
-            cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4]/div/div/table/tbody/tr[3]/td[2]',
-            cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4]/div/div/table/tbody/tr[3]/td[3]'
-            }  
+                cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+                    /div/div/table/tbody/tr[3]/td[1]/div/div[1]',
+                cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+                    /div/div/table/tbody/tr[3]/td[1]/div/div[2]',
+                cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+                    /div/div/table/tbody/tr[3]/td[2]',
+                cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+                    /div/div/table/tbody/tr[3]/td[3]'
+            }
             usd = {}
             for k in curr.keys():
-                usd[k] = (dom.xpath(curr[k])[0].text).strip() 
+                usd[k] = (dom.xpath(curr[k])[0].text).strip()
 
             curr = {
-            cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4]/div/div/table/tbody/tr[4]/td[1]/div/div[1]',
-            cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4]/div/div/table/tbody/tr[4]/td[1]/div/div[2]',
-            cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4]/div/div/table/tbody/tr[4]/td[2]',
-            cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4]/div/div/table/tbody/tr[4]/td[3]'
-            }  
+                cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+                    /div/div/table/tbody/tr[4]/td[1]/div/div[1]',
+                cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+                    /div/div/table/tbody/tr[4]/td[1]/div/div[2]',
+                cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+                    /div/div/table/tbody/tr[4]/td[2]',
+                cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+                    /div/div/table/tbody/tr[4]/td[3]'
+            }
             eur = {}
             for k in curr.keys():
-                eur[k] = (dom.xpath(curr[k])[0].text).strip() 
-            
+                eur[k] = (dom.xpath(curr[k])[0].text).strip()
+
             res = [cny, usd, eur]
-            return res 
+            return res
         else:
             print("Page was not parsed.")
             sys.exit(1)
@@ -79,53 +89,69 @@ def get_metals_data(page_url):
     except requests.exceptions.Timeout:
         print("The request timed out!")
         sys.exit(1)
-    else:    
+    else:
         if page.status_code == 200:
             pageParsed = BeautifulSoup(page.content, 'html5lib')
-            dom = etree.HTML(str(pageParsed)) 
+            dom = etree.HTML(str(pageParsed))
 
             metal = {
-            cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6]/div/div/table/tbody/tr[2]/td[1]/div/div[1]', 
-            cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6]/div/div/table/tbody/tr[2]/td[1]/div/div[2]',
-            cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6]/div/div/table/tbody/tr[2]/td[2]',
-            cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6]/div/div/table/tbody/tr[2]/td[3]'
-            }  
-            Au={}
+                cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+                    /div/div/table/tbody/tr[2]/td[1]/div/div[1]',
+                cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+                    /div/div/table/tbody/tr[2]/td[1]/div/div[2]',
+                cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+                    /div/div/table/tbody/tr[2]/td[2]',
+                cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+                    /div/div/table/tbody/tr[2]/td[3]'
+            }
+            Au = {}
             for k in metal.keys():
-                Au[k] = (dom.xpath(metal[k])[0].text).strip() 
+                Au[k] = (dom.xpath(metal[k])[0].text).strip()
 
             metal = {
-            cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6]/div/div/table/tbody/tr[3]/td[1]/div/div[1]', 
-            cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6]/div/div/table/tbody/tr[3]/td[1]/div/div[2]',
-            cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6]/div/div/table/tbody/tr[3]/td[2]',
-            cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6]/div/div/table/tbody/tr[3]/td[3]'
-            }  
-            Ag={}
+                cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+                    /div/div/table/tbody/tr[3]/td[1]/div/div[1]',
+                cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+                    /div/div/table/tbody/tr[3]/td[1]/div/div[2]',
+                cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+                    /div/div/table/tbody/tr[3]/td[2]',
+                cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+                    /div/div/table/tbody/tr[3]/td[3]'
+            }
+            Ag = {}
             for k in metal.keys():
-                Ag[k] = (dom.xpath(metal[k])[0].text).strip() 
+                Ag[k] = (dom.xpath(metal[k])[0].text).strip()
 
             metal = {
-            cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6]/div/div/table/tbody/tr[4]/td[1]/div/div[1]',
-            cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6]/div/div/table/tbody/tr[4]/td[1]/div/div[2]',
-            cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6]/div/div/table/tbody/tr[4]/td[2]',
-            cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6]/div/div/table/tbody/tr[4]/td[3]'
-            }  
-            Pl={}
+                cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+                    /div/div/table/tbody/tr[4]/td[1]/div/div[1]',
+                cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+                    /div/div/table/tbody/tr[4]/td[1]/div/div[2]',
+                cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+                    /div/div/table/tbody/tr[4]/td[2]',
+                cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+                    /div/div/table/tbody/tr[4]/td[3]'
+            }
+            Pl = {}
             for k in metal.keys():
-                Pl[k] = (dom.xpath(metal[k])[0].text).strip() 
-            
+                Pl[k] = (dom.xpath(metal[k])[0].text).strip()
+
             metal = {
-            cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6]/div/div/table/tbody/tr[5]/td[1]/div/div[1]',
-            cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6]/div/div/table/tbody/tr[5]/td[1]/div/div[2]',
-            cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6]/div/div/table/tbody/tr[5]/td[2]',
-            cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6]/div/div/table/tbody/tr[5]/td[3]'
-            }  
-            Pd={}
+                cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+                    /div/div/table/tbody/tr[5]/td[1]/div/div[1]',
+                cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+                    /div/div/table/tbody/tr[5]/td[1]/div/div[2]',
+                cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+                    /div/div/table/tbody/tr[5]/td[2]',
+                cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+                    /div/div/table/tbody/tr[5]/td[3]'
+            }
+            Pd = {}
             for k in metal.keys():
-                Pd[k] = (dom.xpath(metal[k])[0].text).strip() 
+                Pd[k] = (dom.xpath(metal[k])[0].text).strip()
 
             res = [Au, Ag, Pl, Pd]
-            return res 
+            return res
         else:
             print("Page was not parsed.")
             sys.exit(1)
@@ -137,4 +163,3 @@ def metals():
     # Retrieve data from the first page.
     data = get_metals_data(cbrf_indicators_url)
     return data
-
--- test_cbrf.py
+++ test_cbrf.py
@@ -22,3 +22,27 @@ def test_currencies_eur():
     data = cbrf.currencies()
     print(f'{data}')
     assert data[2]['name'] == '1 EUR'
+
+
+def test_metals_au():
+    data = cbrf.metals()
+    print(f'{data}')
+    assert data[0]['name'] == 'Au'
+
+
+def test_metals_ag():
+    data = cbrf.metals()
+    print(f'{data}')
+    assert data[1]['name'] == 'Ag'
+
+
+def test_metals_pt():
+    data = cbrf.metals()
+    print(f'{data}')
+    assert data[2]['name'] == 'Pt'
+
+
+def test_metals_pd():
+    data = cbrf.metals()
+    print(f'{data}')
+    assert data[3]['name'] == 'Pd'
