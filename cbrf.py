9f72d76f5584a4f66955e729553fffc848a64cc6
Igor Plastov <chetverovod@gmail.com>
2025-01-16 10:30:43 +0300

    Add lxml to pylint white list.

--- cbrf.py
+++ cbrf.py
@@ -1,7 +1,4 @@
-"""Module provides functions for metal, currencies courses reading from site
-https://www.cbr.ru/."""
-
-import sys
+"""Module provides functions for metal, currencies courses from https://www.cbr.ru/."""
 
 import requests
 from bs4 import BeautifulSoup
@@ -30,57 +27,58 @@ def get_currencies_data(page_url) -> dict:
         msg = "The request timed out!"
         raise ValueError(msg) from exc
 
-    if page.status_code == 200:
-        page_parsed = BeautifulSoup(page.content, 'html5lib')
-        dom = etree.HTML(str(page_parsed))
-
-        currency_locators = {
-            cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
-                /div/div/table/tbody/tr[2]/td[1]/div/div[1]',
-            cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
-                /div/div/table/tbody/tr[2]/td[1]/div/div[2]',
-            cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
-                /div/div/table/tbody/tr[2]/td[2]',
-            cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
-                /div/div/table/tbody/tr[2]/td[3]'
-        }
-        cny = {}
-        for key, locator in currency_locators.items():
-            cny[key] = (dom.xpath(locator)[0].text).strip()
-
-        currency_locators = {
-            cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
-                /div/div/table/tbody/tr[3]/td[1]/div/div[1]',
-            cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
-                /div/div/table/tbody/tr[3]/td[1]/div/div[2]',
-            cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
-                /div/div/table/tbody/tr[3]/td[2]',
-            cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
-                /div/div/table/tbody/tr[3]/td[3]'
-        }
-        usd = {}
-        for key, locator in currency_locators.items():
-            usd[key] = (dom.xpath(locator)[0].text).strip()
-
-        currency_locators = {
-            cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
-                /div/div/table/tbody/tr[4]/td[1]/div/div[1]',
-            cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
-                /div/div/table/tbody/tr[4]/td[1]/div/div[2]',
-            cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
-                /div/div/table/tbody/tr[4]/td[2]',
-            cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
-                /div/div/table/tbody/tr[4]/td[3]'
-        }
-        eur = {}
-        for key, locator in currency_locators.items():
-            eur[key] = (dom.xpath(locator)[0].text).strip()
-
-        res = [cny, usd, eur]
-        return res
-    else:
+    if page.status_code != 200:
         msg = "Page was not parsed."
         raise ValueError(msg)
+
+    page_parsed = BeautifulSoup(page.content, 'html5lib')
+    dom = etree.HTML(str(page_parsed))
+
+    currency_locators = {
+        cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+            /div/div/table/tbody/tr[2]/td[1]/div/div[1]',
+        cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+            /div/div/table/tbody/tr[2]/td[1]/div/div[2]',
+        cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+            /div/div/table/tbody/tr[2]/td[2]',
+        cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+            /div/div/table/tbody/tr[2]/td[3]'
+    }
+    cny = {}
+    for key, locator in currency_locators.items():
+        cny[key] = (dom.xpath(locator)[0].text).strip()
+
+    currency_locators = {
+        cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+            /div/div/table/tbody/tr[3]/td[1]/div/div[1]',
+        cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+            /div/div/table/tbody/tr[3]/td[1]/div/div[2]',
+        cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+            /div/div/table/tbody/tr[3]/td[2]',
+        cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+            /div/div/table/tbody/tr[3]/td[3]'
+    }
+    usd = {}
+    for key, locator in currency_locators.items():
+        usd[key] = (dom.xpath(locator)[0].text).strip()
+
+    currency_locators = {
+        cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+            /div/div/table/tbody/tr[4]/td[1]/div/div[1]',
+        cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+            /div/div/table/tbody/tr[4]/td[1]/div/div[2]',
+        cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+            /div/div/table/tbody/tr[4]/td[2]',
+        cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[4] \
+            /div/div/table/tbody/tr[4]/td[3]'
+    }
+    eur = {}
+    for key, locator in currency_locators.items():
+        eur[key] = (dom.xpath(locator)[0].text).strip()
+
+    res = [cny, usd, eur]
+    return res
+
 
 
 def currencies() -> dict:
@@ -108,71 +106,71 @@ def get_metals_data(page_url) -> list:
         msg = "The request timed out!"
         raise ValueError(msg) from exc
 
-    if page.status_code == 200:
-        page_parsed = BeautifulSoup(page.content, 'html5lib')
-        dom = etree.HTML(str(page_parsed))
-
-        metal_locators = {
-            cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
-                /div/div/table/tbody/tr[2]/td[1]/div/div[1]',
-            cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
-                /div/div/table/tbody/tr[2]/td[1]/div/div[2]',
-            cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
-                /div/div/table/tbody/tr[2]/td[2]',
-            cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
-                /div/div/table/tbody/tr[2]/td[3]'
-        }
-        aurum = {}
-        for key, locator in metal_locators.items():
-            aurum[key] = (dom.xpath(locator)[0].text).strip()
-
-        metal_locators = {
-            cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
-                /div/div/table/tbody/tr[3]/td[1]/div/div[1]',
-            cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
-                /div/div/table/tbody/tr[3]/td[1]/div/div[2]',
-            cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
-                /div/div/table/tbody/tr[3]/td[2]',
-            cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
-                /div/div/table/tbody/tr[3]/td[3]'
-        }
-        argentum = {}
-        for key, locator in metal_locators.items():
-            argentum[key] = (dom.xpath(locator)[0].text).strip()
-
-        metal_locators = {
-            cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
-                /div/div/table/tbody/tr[4]/td[1]/div/div[1]',
-            cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
-                /div/div/table/tbody/tr[4]/td[1]/div/div[2]',
-            cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
-                /div/div/table/tbody/tr[4]/td[2]',
-            cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
-                /div/div/table/tbody/tr[4]/td[3]'
-        }
-        platinum = {}
-        for key, locator in metal_locators.items():
-            platinum[key] = (dom.xpath(locator)[0].text).strip()
-
-        metal_locators = {
-            cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
-                /div/div/table/tbody/tr[5]/td[1]/div/div[1]',
-            cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
-                /div/div/table/tbody/tr[5]/td[1]/div/div[2]',
-            cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
-                /div/div/table/tbody/tr[5]/td[2]',
-            cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
-                /div/div/table/tbody/tr[5]/td[3]'
-        }
-        palladium = {}
-        for key, locator in metal_locators.items():
-            palladium[key] = (dom.xpath(locator)[0].text).strip()
-
-        res = [aurum, argentum, platinum, palladium]
-        return res
-    else:
+    if page.status_code != 200:
         msg = "Page was not parsed."
         raise ValueError(msg)
+
+    page_parsed = BeautifulSoup(page.content, 'html5lib')
+    dom = etree.HTML(str(page_parsed))
+
+    metal_locators = {
+        cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+            /div/div/table/tbody/tr[2]/td[1]/div/div[1]',
+        cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+            /div/div/table/tbody/tr[2]/td[1]/div/div[2]',
+        cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+            /div/div/table/tbody/tr[2]/td[2]',
+        cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+            /div/div/table/tbody/tr[2]/td[3]'
+    }
+    aurum = {}
+    for key, locator in metal_locators.items():
+        aurum[key] = (dom.xpath(locator)[0].text).strip()
+
+    metal_locators = {
+        cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+            /div/div/table/tbody/tr[3]/td[1]/div/div[1]',
+        cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+            /div/div/table/tbody/tr[3]/td[1]/div/div[2]',
+        cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+            /div/div/table/tbody/tr[3]/td[2]',
+        cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+            /div/div/table/tbody/tr[3]/td[3]'
+    }
+    argentum = {}
+    for key, locator in metal_locators.items():
+        argentum[key] = (dom.xpath(locator)[0].text).strip()
+
+    metal_locators = {
+        cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+            /div/div/table/tbody/tr[4]/td[1]/div/div[1]',
+        cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+            /div/div/table/tbody/tr[4]/td[1]/div/div[2]',
+        cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+            /div/div/table/tbody/tr[4]/td[2]',
+        cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+            /div/div/table/tbody/tr[4]/td[3]'
+    }
+    platinum = {}
+    for key, locator in metal_locators.items():
+        platinum[key] = (dom.xpath(locator)[0].text).strip()
+
+    metal_locators = {
+        cn[0]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+            /div/div/table/tbody/tr[5]/td[1]/div/div[1]',
+        cn[1]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+            /div/div/table/tbody/tr[5]/td[1]/div/div[2]',
+        cn[2]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+            /div/div/table/tbody/tr[5]/td[2]',
+        cn[3]: '//*[@id="content"]/div/div/div/div[2]/div[2]/div[6] \
+            /div/div/table/tbody/tr[5]/td[3]'
+    }
+    palladium = {}
+    for key, locator in metal_locators.items():
+        palladium[key] = (dom.xpath(locator)[0].text).strip()
+
+    res = [aurum, argentum, platinum, palladium]
+    return res
 
 
 def metals():
